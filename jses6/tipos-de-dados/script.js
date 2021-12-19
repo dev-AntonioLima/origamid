5ce098bd0db66bea3d4c7308de47f0d8cf13ce9e
Antonio Lima <antonio.lima@metatron.com.br>
2021-12-19 02:12:19 -0300

    Tipos de Dados

---
+++ jses6/tipos-de-dados/script.js
@@ -0,0 +1,27 @@
+var nome = 'Antonio ';
+var sobrenome = 'Lima';
+var nomeCompleto = nome + sobrenome; 
+var idade = 28;
+var time;
+var simbolo = Symbol();
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+var ano = '2018';
+var mes = 8;
+var frase1 = 'Esse é o \'melhor\' jogo'
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(typeof nome);
+console.log(typeof time);
+console.log(typeof simbolo);
+console.log (nomeCompleto);
+console.log(typeof frase);
+console.log(ano + mes); 
+
+// Exercício
+
+var caractere = 'caractere';
+var numString = '1';
+var time = `It's time`;
+
+console.log(typeof nomeCompleto);
