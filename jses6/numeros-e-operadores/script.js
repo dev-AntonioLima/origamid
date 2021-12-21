6b60874a9e57b1eb0c8a29593c1a68a320ea8f72
Antonio Lima <antonio.lima@metatron.com.br>
2021-12-21 11:48:06 -0300

    Números e Operadores 2

--- jses6/numeros-e-operadores/script.js
+++ jses6/numeros-e-operadores/script.js
@@ -2,7 +2,48 @@ var total = 10 + 5 + 10;
 var divisao = 200 / 5;
 var modulo = 3872983892 % 3;
 
-
 console.log(total);
 console.log(divisao);
 console.log(modulo);
+
+var testeNaN = '100' / 2;
+console.log(isNaN(testeNaN));
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(++x);
+console.log(x);
+
+var frase = 'Isso é um teste';
++frase;
+-frase;
+
+var idade = '28';
++idade;
+-idade;  
+
+console.log(+idade + 5);
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade);
+
+// Exercício
+
+var total = 10 + 5 * 2 / 2 + 20;
+// 35
+
+var not = 'not';
+console.log(+not);
+console.log(-not);
+
+console.log(+'200' + 50);
+
+console.log(x);
+
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade;
+
+console.log(peso);
