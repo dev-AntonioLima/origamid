97bd2f35b9f6317e7edaff64375936fac0f439f1
Antonio Lima <antonio.lima@metatron.com.br>
2021-12-23 22:02:44 -0300

    Funções 2

--- jses6/funcoes/script.js
+++ jses6/funcoes/script.js
@@ -33,3 +33,45 @@ function corFavorita(cor) {
 console.log(corFavorita('verde'));
 
 addEventListener('click', function(){ console.log('Oi') });
+
+function imc2(peso, altura) {
+    const imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+console.log(imc2(20, 1.8));
+
+function terceiraIdade(idade) {
+    if(typeof idade !== 'number'){
+        return 'Por favor preencha um número'
+    } else if (idade >= 60 ){
+       return true;
+    }  else {
+        return false;
+    }
+    console.log('testando') // return 
+}
+
+terceiraIdade(60);
+
+function faltaVisitar(paisesVisitados) {
+    var totalPaises = 193;
+    return `Faltam visitar ${totalPaises - paisesVisitados}`;
+}
+
+console.log(faltaVisitar(93));
+
+var profissao = 'Programador';
+
+function dados() {
+    var nome = 'Antonio';
+    var idade = 23;
+    function outrosDados(){
+        var endereco = 'Rio de Janeiro';
+        var idade = 29;
+        return `${nome}`, `${idade}`, `${endereco}` , `${profissao}`;
+    }
+    return outrosDados();
+}
+
+console.log(dados());
