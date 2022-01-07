b8a117dc7acc044f319ae0c19b01832f0d7478ca
Antonio Lima <antonio.lima@metatron.com.br>
2022-01-07 12:04:54 -0300

    Objetos 2

--- jses6/objetos/script.js
+++ jses6/objetos/script.js
@@ -20,3 +20,55 @@ console.log(quadrado.area(10));
 console.log(quadrado.perimetro(5))
 console.table(quadrado);
 
+var height = 100;
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: '#84E',
+    metadeHeight() {
+        return this.height / 2;
+    }
+}
+
+menu.backgroundColor = '#000'
+
+menu.color = 'blue';
+var bg = menu.backgroundColor;
+
+menu.esconder = function(){
+    console.log('Escondido');
+}
+
+
+// Crie um objeto com seus dados pessoais
+var eu = {
+  nome: 'Antonio',
+  sobrenome: 'Lima',
+  idade: 23,
+  escolaridade: 'Superior Incompleto',
+  temEmprego: true,
+  exibeNome(){
+      console.log(this.nome + ' ' + this.sobrenome);
+  }
+}
+
+eu.exibeNome();
+
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+}
+
+var cachorro = {
+    raca: 'Labrador',
+    cor: 'Preto',
+    idade: 10,
+    latir(pessoa) {
+        if(pessoa ===  'homem'){
+            return 'Latir';
+        } else {
+            return 'Nada';
+        }
+    }
+}
