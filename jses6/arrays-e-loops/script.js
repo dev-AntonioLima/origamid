e8acdfc8c3c60f1160adfb09fc4d0f33f692ef18
Antonio Lima <antonio55senac@gmail.com>
2022-01-11 13:06:33 -0300

    Arrays e Loops 2

--- jses6/arrays-e-loops/script.js
+++ jses6/arrays-e-loops/script.js
@@ -9,3 +9,41 @@ for (i = i; i < 10; i++){
     console.log('Antonio');
  }
 }
+
+var i = 0;
+while (i <= 10) {
+    console.log(i);
+    i = i + 5;
+} 
+
+for ( var i = 0; i< videoGames.length; i++) {
+    console.log(videoGames[i]);
+    if(videoGames[i] === 'PS4') {
+        break; 
+    }
+}
+
+videoGames.forEach(function(i, index){
+    console.log(i,[i.length ], index);
+});
+
+// Crie um array com os anos que o Brasil ganhou a copa
+ var brasilCopa = [1959, 1962, 1970, 1994, 2002];
+
+for(i = 0; i < 1; i++){
+    brasilCopa.forEach(function(index){
+        console.log(' O Brasil ganhou a copa de', `${index}`);
+    });
+}
+
+ var frutas = ['Banana', 'Maca', 'Pera', 'Uva', 'Melancia'];
+
+ for (i = 0; i < frutas.length; i++) {
+   console.log(frutas[i]);
+   if(frutas[i] === 'Pera'){
+       break;
+   }
+}
+
+var ultimaFruta = frutas[frutas.length - 1];
+console.log(ultimaFruta);
