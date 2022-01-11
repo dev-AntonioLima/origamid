1e0ddb94ce2688b6330867a6b20abd6ea3cefd36
Antonio Lima <antonio55senac@gmail.com>
2022-01-11 00:34:06 -0300

    Arrays e Loops 1

---
+++ jses6/arrays-e-loops/script.js
@@ -0,0 +1,11 @@
+var videoGames = ['Switch', 'PS4', 'Xbox'];
+
+videoGames.pop();
+videoGames.push('3DS');
+videoGames.length;
+
+function loop(i){
+for (i = i; i < 10; i++){
+    console.log('Antonio');
+ }
+}
