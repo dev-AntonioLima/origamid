91b928fa14b31e8aaf1a3a1fd8099dab4d5a0d85
Antonio Lima <antonio55senac@gmail.com>
2022-01-20 10:47:41 -0300

    O que é o DOM 1

---
+++ jses6/DOM-PARA-INICIANTES/o-que-e-o-dom/script.js
@@ -0,0 +1,10 @@
+window.alert('ISSO');
+
+const href = window.location.href;
+
+console.log(href);
+
+if(href === 'http://127.0.0.1:5500/DOM-PARA-INICIANTES/o-que-e-o-dom/index.html')
+console.log('Essa pagina');
+
+const h1Selecionado = document.querySelector('h1');
