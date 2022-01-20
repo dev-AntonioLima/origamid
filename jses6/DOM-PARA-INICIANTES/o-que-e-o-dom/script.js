84afebaf9f23159e90514d01f5293b88134bd803
Antonio Lima <antonio55senac@gmail.com>
2022-01-20 12:19:37 -0300

    O que é o DOM 2

--- jses6/DOM-PARA-INICIANTES/o-que-e-o-dom/script.js
+++ jses6/DOM-PARA-INICIANTES/o-que-e-o-dom/script.js
@@ -8,3 +8,16 @@ if(href === 'http://127.0.0.1:5500/DOM-PARA-INICIANTES/o-que-e-o-dom/index.html'
 console.log('Essa pagina');
 
 const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+function callbackh1() {
+    console.log('Clicou em ', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1);
+
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+const larguraPag = window.innerWidth;
+console.log(larguraPag);
